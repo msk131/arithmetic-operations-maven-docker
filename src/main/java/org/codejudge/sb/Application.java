01dd339ba83e05449183b566594d8c3cf3d24a09
Mani Shankar Kumar <manishankarkumar@Manis-MacBook-Air.local>
2020-08-01 16:22:18 +0530

    first code

--- src/main/java/org/codejudge/sb/Application.java
+++ src/main/java/org/codejudge/sb/Application.java
@@ -1,6 +1,9 @@
 package org.codejudge.sb;
 
 import lombok.extern.slf4j.Slf4j;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -9,9 +12,12 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan({"org.codejudge.sb"})
 @Slf4j
 public class Application {
+	
+	private static final Logger logger = LoggerFactory.getLogger("Application.class");
 
     public static void main(String[] args) {
-        log.info("Starting Application...");
+        
+        logger.info("Starting Application...");
         SpringApplication.run(Application.class, args);
     }
 }
--- src/main/java/org/codejudge/sb/controller/AppController.java
+++ src/main/java/org/codejudge/sb/controller/AppController.java
@@ -1,18 +1,29 @@
 package org.codejudge.sb.controller;
 
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.GetMapping;
+
+import org.codejudge.sb.model.NumbersWrapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping
 public class AppController {
 
-    @ApiOperation("This is the hello world api")
-    @GetMapping("/")
-    public String hello() {
-        return "Hello World!!";
+    @ApiOperation("Add Two Numbers")
+    @RequestMapping(value= {"/api/add"}, method = RequestMethod.POST, produces="application/json",
+    consumes = "application/json")
+    @ResponseBody
+    public ResponseEntity<Integer> addTwoNumbers(@RequestBody NumbersWrapper numberWrapper) {
+    	
+    	int sum = numberWrapper.getNumber1() + numberWrapper.getNumber2();
+    	   	
+        return new ResponseEntity<Integer>(sum, HttpStatus.OK);
     }
 
 }
